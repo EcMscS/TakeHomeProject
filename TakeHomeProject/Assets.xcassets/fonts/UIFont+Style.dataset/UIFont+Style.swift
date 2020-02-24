251eccd8dea02360eb3e5310b241f0c6919d2018
Arin Davoodian <arindavoodian@gmail.com>
2020-02-24 08:25:40 -0800

    added initial structure to project. added working assets to project.

---
+++ TakeHomeProject/Assets.xcassets/fonts/UIFont+Style.dataset/UIFont+Style.swift
@@ -0,0 +1,34 @@
+//
+//  UIFont+Style.swift
+//  Mizo
+//
+//  Created by Dave Poirier on 2019-10-22.
+//  Copyright © 2019 Mizo LLC. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Export from Zeplin
+extension UIFont {
+    
+    class var largeTitle: UIFont {
+      return UIFont(name: "ApercuPro-Bold", size: 24.0)!
+    }
+
+    class var sectionHeader: UIFont {
+      return UIFont(name: "ApercuPro-Bold", size: 16.0)!
+    }
+
+    class var title: UIFont {
+      return UIFont(name: "ApercuPro-Bold", size: 16.0)!
+    }
+
+    class var subTitle: UIFont {
+      return UIFont(name: "ApercuPro-Regular", size: 16.0)!
+    }
+    
+    class var guidanceText: UIFont {
+      return UIFont(name: "ApercuPro-Bold", size: 12.0)!
+    }
+}
+
