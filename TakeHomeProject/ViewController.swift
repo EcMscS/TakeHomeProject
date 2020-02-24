ea97403022c879b3a09dcd3c4f0e3e5e92b9a599
Arin Davoodian <arindavoodian@gmail.com>
2020-02-24 08:18:11 -0800

    Initial Commit

---
+++ TakeHomeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TakeHomeProject
+//
+//  Created by Arin Davoodian on 2/24/20.
+//  Copyright © 2020 Arin Davoodian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
