709b034f7d6d6b93e4acc79451e825eac3ba6456
Arin Davoodian <arindavoodian@gmail.com>
2020-02-25 07:06:32 -0800

    added location services and location updates

--- TakeHomeProject/Search Controllers/SearchViewController.swift
+++ TakeHomeProject/Search Controllers/SearchViewController.swift
@@ -7,15 +7,55 @@
 //
 
 import UIKit
+import CoreLocation
+import Mapbox
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, MGLMapViewDelegate {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var mapView: MGLMapView!
+	
+	var locationManager: CLLocationManager?
+	
+	var userLocation: CLLocationCoordinate2D?
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+		
+		setupLocationServices()
+		setupMapView()
     }
-    
+	
+	func setupLocationServices() {
+		locationManager = CLLocationManager()
+		locationManager?.delegate = self
+		locationManager?.requestWhenInUseAuthorization()
+		locationManager?.desiredAccuracy = kCLLocationAccuracyHundredMeters
+		locationManager?.requestLocation()
+	}
+	
+	func setupMapView() {
+		mapView.delegate = self
+	}
+	
+	func setMapViewCenter() {
+		guard let userLocation = userLocation else { return }
+		mapView.setCenter(userLocation, zoomLevel: 13, animated: true)
+	}
 
+}
 
+extension SearchViewController: CLLocationManagerDelegate {
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		guard let latestLocation = locations.last else {
+			print("no location")
+			return
+		}
+		userLocation = CLLocationCoordinate2D(latitude: latestLocation.coordinate.latitude, longitude: latestLocation.coordinate.longitude)
+		setMapViewCenter()
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+		print(error)
+	}
 }
