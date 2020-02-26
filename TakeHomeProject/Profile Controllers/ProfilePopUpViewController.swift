980ed0bd7c5eca78f288e2da766c52fa5c80b99b
Arin Davoodian <arindavoodian@gmail.com>
2020-02-26 08:01:03 -0800

    added profilePopUp view controller

---
+++ TakeHomeProject/Profile Controllers/ProfilePopUpViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ProfilePopUpViewController.swift
+//  TakeHomeProject
+//
+//  Created by Arin Davoodian on 2/26/20.
+//  Copyright © 2020 Arin Davoodian. All rights reserved.
+//
+
+import UIKit
+
+class ProfilePopUpViewController: UIViewController {
+
+	@IBOutlet weak var cellPhoneTextField: UITextField!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+       cellPhoneTextField.addLine(position: .lineAtBottom, color: .white, width: 2)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TakeHomeProject/Profile Controllers/ProfileViewController.swift
+++ TakeHomeProject/Profile Controllers/ProfileViewController.swift
@@ -11,23 +11,11 @@ import UIKit
 class ProfileViewController: UIViewController {
 
 	
-	@IBOutlet weak var cellPhoneTextField: UITextField!
-	
     override func viewDidLoad() {
         super.viewDidLoad()
 
-		cellPhoneTextField.addLine(position: .lineAtBottom, color: .white, width: 2)
+		
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
