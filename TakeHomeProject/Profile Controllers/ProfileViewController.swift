f3dfd4eab3cb23249915aa29c84a58e31f329d7b
Arin Davoodian <arindavoodian@gmail.com>
2020-02-26 08:18:08 -0800

    added Signin Auth check structure and views

--- TakeHomeProject/Profile Controllers/ProfileViewController.swift
+++ TakeHomeProject/Profile Controllers/ProfileViewController.swift
@@ -14,8 +14,17 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+		DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+			self.checkAuthStatus()
+		}
 		
     }
     
-
+	func checkAuthStatus() {
+		let storyboard = UIStoryboard(name: "Profile", bundle: nil)
+		guard let vc = storyboard.instantiateViewController(withIdentifier: "profilePopUp") as? ProfilePopUpViewController else { return }
+		vc.modalPresentationStyle = .overFullScreen
+		vc.modalTransitionStyle = .crossDissolve
+		present(vc, animated: true, completion: nil)
+	}
 }
